7ebe711b9e408d15c6daeca3ccd48f6c1c9772a4
LA\Gerardo Garita <ggaritajuarez@gmail.com>
2018-08-31 10:03:34 -0600

    GGarita, content menu - pendiente limpiar html

--- example/js/contentMenu.js
+++ example/js/contentMenu.js
@@ -8,11 +8,14 @@
 ///////////////////////////////////////////////
 
 ; (function ($) {
+    var _opts;
+    jQuery.fn.ContentMenu = function () {
+        return this;
+    };
     jQuery.fn.ggContentMenu = function (options) {
         try {
             var _menu = this;
             var _height = $(window).height();
-
             $(_menu).find("a").each(function (p, atag) {
                 $(atag).on("click", function (e) {
                     if ($(this).hasClass("active")) {
@@ -22,7 +25,6 @@
                     }
                 });
             });
-
             $('html').unbind("click").on('click', function (event) {
                 if ($(_menu).find($(event.target)).length <= 0) {
                     $(".ggContentMenu div.contentMenu.first").animate({ width: "0px" }, 700);
@@ -34,32 +36,45 @@
                     $(".ggContentMenu div.contentMenu.first").css("width", "260px");
                 }
             });
-
-            if ((options != undefined) && (options !== null) && (options !== "")) {
-                if (options.hasOwnProperty("top")) {
-                    $(".ggContentMenu div.contentMenu.first").css("top", options.top);
-                    _height -= (options.top);
+            $(this).ContentMenu().Refresh(options);
+            console.log("gg:content menu ready!");
+        }
+        catch (err) {
+            console.log("Error: " + err + ".");
+        }
+        finally {
+            setTimeout(function () {
+                window.dispatchEvent(new Event('resize'));
+            }, 1000);
+        }
+    };
+    jQuery.fn.ContentMenu().Refresh = function (options) {
+        try {
+            var _height = $(window).height();
+            _opts = ((options == undefined || options === null || options === "") ? _opts : options);
+            if ((_opts != undefined) && (_opts !== null) && (_opts !== "")) {
+                if (_opts.hasOwnProperty("top")) {
+                    $(".ggContentMenu div.contentMenu.first").css("top", _opts.top);
+                    _height -= (_opts.top);
                 } else {
                     $(".ggContentMenu div.contentMenu.first").css("top", 0).css("border-top", "0px");
                 }
-                if (options.hasOwnProperty("bottom")) {
-                    $(".ggContentMenu div.contentMenu.first").css("bottom", options.bottom);
-                    _height -= (options.bottom);
+                if (_opts.hasOwnProperty("bottom")) {
+                    $(".ggContentMenu div.contentMenu.first").css("bottom", _opts.bottom);
+                    _height -= (_opts.bottom);
                 } else {
                     $(".ggContentMenu div.contentMenu.first").css("bottom", 0).css("border-bottom", "0px");
                 }
-
-                if (options.hasOwnProperty("left")) {
-                    $(".ggContentMenu div.contentMenu.first").css("left", options.left).css("border-left", "0px");
+                if (_opts.hasOwnProperty("left")) {
+                    $(".ggContentMenu div.contentMenu.first").css("left", _opts.left).css("border-left", "0px");
                 } else {
                     $(".ggContentMenu div.contentMenu.first").css("left", 0);
                 }
-                if (options.hasOwnProperty("right")) {
-                    $(".ggContentMenu div.contentMenu.first").css("right", options.right).css("border-right", "0px");
+                if (_opts.hasOwnProperty("right")) {
+                    $(".ggContentMenu div.contentMenu.first").css("right", _opts.right).css("border-right", "0px");
                 }
-                
-                if (options.hasOwnProperty("height")) {
-                    $(".ggContentMenu div.contentMenu.first").css("max-height", options.height);
+                if (_opts.hasOwnProperty("height")) {
+                    $(".ggContentMenu div.contentMenu.first").css("max-height", _opts.height);
                 } else {
                     $(".ggContentMenu div.contentMenu.first").css("max-height", _height);
                 }
@@ -70,8 +85,6 @@
                 $(".ggContentMenu div.contentMenu.first").css("max-height", _height);
                 $(".ggContentMenu div.contentMenu.first").css("z-index", 100);
             }
-            
-            console.log("gg:content menu ready!");
         }
         catch (err) {
             console.log("Error: " + err + ".");
--- example/js/myscript.js
+++ example/js/myscript.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("ul#myMenu").ggContentMenu({
-        top: 50,
-        bottom: 40
+        top: 50, //nav var
+        bottom: 40 //footer
     });
 });
